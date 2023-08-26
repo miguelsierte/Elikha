0b883692c6a7e3925f943dc1b373afe319d24ea7
John Raven <jrglomar016@gmail.com>
2023-08-26 15:16:06 +0800

    changes on home.blade.php updating it for demo purposes

--- resources/views/artist/home.blade.php
+++ resources/views/artist/home.blade.php
@@ -1,18 +1,20 @@
 @extends ('artistlayouts.master')
 @section('header')
-        
+
 @include('artistinc.navbar')
 @include('artistinc.popup')
         <title>Home - @yield('title')</title>
 @endsection
-  
+
+        {{-- THIS IS MY CHANGES - RAVEN --}}
+
         @section('body')
-      
+
         <div class="row">
             <div class="col-4 col-lg-3">
                @include('artistinc.sidebar')
             </div>
-            
+
             <div class="col-8 col-lg-9">
                 <span class="border">
                 <div class="p-3 pb-5 bg-white rounded">
@@ -23,7 +25,7 @@
                         <div class="col-sm-4 p-2 text-align: center">
                             <h2>---</h2>
                             <p>Active Auctions</p>
-                            
+
                         </div>
                         <div class="col-sm-4 p-2 text-align: center">
                             <h2>---</h2>
@@ -33,15 +35,15 @@
                             <h2>---</h2>
                             <p>Important Messages</p>
                         </div>
-                        
+
                       </div>
                 </div>
-                
+
             </span>
-            
+
                     <br>
                     <div class="row grid text-center">
-                        <div class="col-sm-4 p-2"><h4>Engagements</h4>  
+                        <div class="col-sm-4 p-2"><h4>Engagements</h4>
                         <img class="engage" src="artistimg/icon metro-chart-line.png" alt="..." height="105" width="105">
                         <div class="m-1">
                         <h3>---</h3>
@@ -55,7 +57,7 @@
                             <p>have viewed your profile</p>
                         </div>
                         </div>
-                        
+
                         <div class="col-sm-4 p-2"><h4>Sold</h4>
                             <img class="sold" src="artistimg/icon metro-money.png" alt="..." height="100" width="70">
                             <div class="m-1">
@@ -63,11 +65,11 @@
                             <p>sold artworks</p>
                         </div>
                         </div>
-                      </div>     
+                      </div>
             </div>
         </span>
         </div>
         </div>
         </div>
-        
+
         @endsection
